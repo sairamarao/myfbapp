2aea9f6a22274e84fe66ea404f3b561d0602d9ae
sairamarao <sairamarao.aitharaju@gmail.com>
2012-06-14 12:29:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfbapp::Application.config.secret_token = '2124ff7f8d401b2e6276ae2abba4b378261c6523a9375d8ea4b3c1d90c86bb49718a9953d683a000cbd935fa4b02892969deb07560b26e2c6eb1ffc5ee890249'
